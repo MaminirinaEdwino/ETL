98ef245bb47c474155a97c53dc2ab9dcd58414fc
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-03-10 11:19:41 +0300

    fix: separate transform code

--- main.go
+++ main.go
@@ -12,41 +12,7 @@ import (
 )
 
 
-func transformRow(row []string, e *Extractor) (RawAccident, error) {
-	latStr := e.GetValue(row, "Latitude")
-	lngStr := e.GetValue(row, "Longitude")
-	sevStr := e.GetValue(row, "Accident_severity")
-	dateStr := e.GetValue(row, "Accident date")
-	timeStr := e.GetValue(row, "Time")
-	vTypeStr := e.GetValue(row, "vehicle_type")
-	speedStr := e.GetValue(row, "speed_limit")
-	casualStr := e.GetValue(row, "Number_of_casualities")
-	day_of_week := e.GetValue(row, "Day_of_Week")
-	nbr_vehicle := e.GetValue(row, "Number_of_Vehicles")
-	//conversion
-	lat, _ := strconv.ParseFloat(latStr, 64)
-	lng, _ := strconv.ParseFloat(lngStr, 64)
-	speed, _ := strconv.Atoi(speedStr)
-	casual, _ := strconv.Atoi(casualStr)
-	vehicle, _ := strconv.Atoi(nbr_vehicle)
-	severity := strings.ToUpper(strings.TrimSpace(sevStr))
 
-	// 4. Retourner l'objet structuré
-	return RawAccident{
-		Index:       e.GetValue(row, "Accident_Index"),
-		Date:        dateStr,
-		Time:        timeStr,
-		Severity:    severity,
-		Latitude:    lat,
-		Longitude:   lng,
-		SpeedLimit:  speed,
-		Casualties:  casual,
-		Weather:     e.GetValue(row, "weather_conditions"),
-		VehicleType: vTypeStr,
-		DayOfWeek:   day_of_week,
-		Vehicles:    vehicle,
-	}, nil
-}
 
 func loadToJSON(filename string, accidents <-chan RawAccident) error {
 	file, err := os.Create(filename)
