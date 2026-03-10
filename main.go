3e4f3df5843623e59bd26504f788c061ecba2dd8
Maminirina Edwino <edwinomaminirina@gmail.com>
2026-03-10 11:22:14 +0300

    add: load method

--- main.go
+++ main.go
@@ -1,39 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"os"
 )
 
 
 
 
-func loadToJSON(filename string, accidents <-chan RawAccident) error {
-	file, err := os.Create(filename)
-	if err != nil {
-		return fmt.Errorf("erreur création fichier: %v", err)
-	}
-	defer file.Close()
 
-	encoder := json.NewEncoder(file)
-	count := 0
-	for acc := range accidents {
-		err := encoder.Encode(acc)
-		if err != nil {
-			fmt.Printf("Erreur encodage accident %s: %v\n", acc.Index, err)
-			continue
-		}
-
-		count++
-		if count%1000 == 0 {
-			fmt.Printf("Chargement : %d accidents sauvegardés...\n", count)
-		}
-	}
-
-	fmt.Printf("Chargement terminé : %d accidents écrits dans %s\n", count, filename)
-	return nil
-}
 
 
 
